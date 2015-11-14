fc1cccfc48c992b357533488ce1ad3d0082603ff
Alex-Chizhov <alexchizhov90@gmail.com>
2015-11-14 20:40:22 +0300

    Задание 3

---
+++ home_work_3/info_contact.py
@@ -0,0 +1,33 @@
+__author__ = 'Alex'
+
+class Infos:
+
+    def __init__(self, firstname,middelname,lastname,nickname, title,company,addres,home,mobile,
+                          fax,homepage,day_Birthday,month_Birthday,year_Birthday,day_Anniversary,
+                          month_Anniversary,year_Anniversary,address2,phone2,notes,work):
+
+
+        self.firstname         = firstname
+        self.middelname        = middelname
+        self.lastname          = lastname
+        self.nickname          = nickname
+        self.title             = title
+        self.company           = company
+        self.addres            = addres
+        self.home              = home
+        self.mobile            = mobile
+        self.fax               = fax
+        self.homepage          = homepage
+        self.day_Birthday      = day_Birthday
+        self.month_Birthday    = month_Birthday
+        self.year_Birthday     = year_Birthday
+        self.day_Anniversary   = day_Anniversary
+        self.month_Anniversary = month_Anniversary
+        self.year_Anniversary  = year_Anniversary
+        self.address2          = address2
+        self.phone2            = phone2
+        self.notes             = notes
+        self.work              = work
+
+
+
