ae378b96581bd19f59b69e97127a9192769d6070
Alex-Chizhov <alexchizhov90@gmail.com>
2015-11-15 22:17:56 +0300

    Исправлен путь до файла avatar.jpg

--- home_work_3/test_add_group_and_contact.py
+++ home_work_3/test_add_group_and_contact.py
@@ -151,7 +151,7 @@ class test_add_group(unittest.TestCase):
        self.fill_form_contact(wd, Infos(firstname="qq", middelname="qq", lastname="qq", nickname="qq", title="qq", company="qq",
                           addres="ww", home="11", mobile="22", fax="22", homepage="wewr.ru", day_Birthday="[7]",
                           month_Birthday="[10]", year_Birthday="1980", day_Anniversary="[18]", month_Anniversary="[7]",
-                          year_Anniversary="2000", address2="12", phone2="12", notes="12", work ='qwe',photo ="C:\\Users\\Alex\\Documents\\GitHub\\python_training\\test_s_photo\\avatar.jpg"))
+                          year_Anniversary="2000", address2="12", phone2="12", notes="12", work ='qwe',photo ="C:\\Users\\Alex\\Documents\\GitHub\\python_training\\home_work_3\\avatar.jpg"))
        self.input_save_form(wd)
        self.logout(wd)
 
