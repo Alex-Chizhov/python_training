ce4c0c8ce7cfda5bf1aed767e402767c70d0ceb2
Alex-Chizhov <alexchizhov90@gmail.com>
2015-11-26 15:14:24 +0300

    Общая для всех тестов фикстура

---
+++ home_work_6/test/test_del_group.py
@@ -0,0 +1,8 @@
+
+def test_delite_group(app):
+	app.session.login( username="admin", password="secret")
+	app.group.delete_first_group()
+	app.session.logout()
+
+if __name__ == '__main__':
+	pytest.main('test_del_group.py')
